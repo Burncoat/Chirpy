f93b05848556273cf9387b9336027f401f5c849a
Burncoat <htvick@gmail.com>
2025-06-16 15:11:28 -0400

    Added a hit and reset handler

--- main.go
+++ main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"fmt"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+		fileserverHits atomic.Int32
+	}
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
-	mux := http.NewServeMux()
+	
 
 	// Pulling from index.html
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux := http.NewServeMux()
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -24,8 +36,15 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
