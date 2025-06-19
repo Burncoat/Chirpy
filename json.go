5761c4297c8b74572be309ae8f78a3fbcc54315c
Burncoat <htvick@gmail.com>
2025-06-19 13:06:57 -0400

    Added password fields and hashing

--- json.go
+++ json.go
@@ -24,11 +24,11 @@ func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
 	}
 	if code > 499 {
 		log.Printf("Responding with 5XX error: %s", msg)
+	}
 	type errorResponse struct {
 		Error string `json:"error"`
 	}
 	respondWithJSON(w, code, errorResponse{
 		Error: msg,
 	})
-	}
 }
