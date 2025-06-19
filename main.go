e963d0acacfc68467aeb222c2fe2279fa4568830
Burncoat <htvick@gmail.com>
2025-06-18 21:40:28 -0400

    Added a handler to get single chirps by ID

--- main.go
+++ main.go
@@ -49,6 +49,8 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpCreate)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsGet)
+	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpGetByID)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 
